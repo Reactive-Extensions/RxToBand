9811f693c3e347ed4519c80452582c487f7d5248
Bart De Smet <bartde@microsoft.com>
2015-02-27 17:01:04 -0800

    Adding Rx to Band support.

---
+++ RxToBand/Microsoft.Band.Reactive/BandObservableExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Band.Sensors;
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Microsoft.Band.Reactive
+{
+    /// <summary>
+    /// Provides a set of extension methods to enable using Reactive Extensions with the Microsoft Band SDK.
+    /// </summary>
+    public static class BandObservableExtensions
+    {
+
+        /// <summary>
+        /// Convert an observable sequence of Band sensor readings into a timestamped sequence.
+        /// </summary>
+        /// <typeparam name="T">Type of the Band sensor readings exposed by the observable sequence.</typeparam>
+        /// <param name="sensor">The Band sensor observable sequence to convert.</param>
+        /// <returns>Observable sequence of timestamped Band sensor readings.</returns>
+        public static IObservable<Timestamped<T>> ToTimestamped<T>(this IObservable<T> sensor)
+            where T : IBandSensorReading
+        {
+            if (sensor == null)
+            {
+                throw new ArgumentNullException("sensor");
+            }
+
+            //
+            // Just extract the timestamp and promote it to the Rx struct.
+            //
+            return sensor.Select(v => new Timestamped<T>(v, v.Timestamp));
+        }
+
+        /// <summary>
+        /// Creates an observable sequence that only receives Band sensor readings when the Band is worn by the user.
+        /// </summary>
+        /// <typeparam name="T">Type of the Band sensor readings exposed by the observable sequence.</typeparam>
+        /// <param name="sensor">The Band sensor observable sequence to receive readings from when the Band is worn by the user.</param>
+        /// <param name="contact">The observable sequence for the contact sensor of the Band.</param>
+        /// <returns>Observable sequence that only receives Band sensor readings when the Band is worn by the user.</returns>
+        public static IObservable<T> OnlyWhenWorn<T>(this IObservable<T> sensor, IObservable<IBandContactReading> contact)
+        {
+            if (sensor == null)
+            {
+                throw new ArgumentNullException("sensor");
+            }
+
+            if (contact == null)
+            {
+                throw new ArgumentNullException("contact");
+            }
+
+            //
+            // Switch between the specified sensor and the never sequence based on changes to contact state.
+            //
+            return contact.Select(c => c.State == BandContactState.Worn ? sensor : Observable.Never<T>()).Switch();
+        }
+
+    }
+}
---
+++ RxToBand/Microsoft.Band.Reactive/BandSensorExtensions.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Band.Sensors
+{
+    /// <summary>
+    /// Provides a set of extension methods to enable using Reactive Extensions with the Microsoft Band SDK.
+    /// </summary>
+    public static class BandSensorExtensions
+    {
+        /// <summary>
+        /// Converts a Band sensor to an observable sequence.
+        /// </summary>
+        /// <typeparam name="T">Type of the Band sensor readings exposed by the observable sequence.</typeparam>
+        /// <param name="sensor">The Band sensor to obtain sensor readings from.</param>
+        /// <param name="startAndStopSensorReadings">If set to <c>true</c>, the lifecycle of the resulting observable sequence manages starting and stopping the readings from the sensor; if set to <c>false</c>, the caller is responsible to manage the lifetime of sensor readings manually.</param>
+        /// <returns>Observable sequence exposing the Band sensor readings.</returns>
+        public static IObservable<T> ToObservable<T>(this IBandSensor<T> sensor, bool startAndStopSensorReadings = true)
+            where T : IBandSensorReading
+        {
+            if (sensor == null)
+            {
+                throw new ArgumentNullException("sensor");
+            }
+
+            //
+            // Task to track an outstanding stop operation, if any. This is used to ensure
+            // sequential execution of start and stop requests.
+            //
+            var stopping = (Task)Task.FromResult(true);
+
+            //
+            // Observable wrapper around the ReadingChanged event for the Band sensor. It
+            // differs from FromEvent behavior in that it can optionally deal with lifecycle
+            // management to start and stop sensor readings.
+            //
+            var res = Observable.Create<T>(async (observer, ct) =>
+            {
+                EventHandler<BandSensorReadingEventArgs<T>> h = (o, e) =>
+                {
+                    observer.OnNext(e.SensorReading);
+                };
+
+                sensor.ReadingChanged += h;
+
+                if (startAndStopSensorReadings)
+                {
+                    //
+                    // If we have an outstanding stop request, await it to avoid races.
+                    //
+                    await stopping.ConfigureAwait(false);
+
+                    //
+                    // Do the proper start.
+                    //
+                    try
+                    {
+                        await sensor.StartReadingsAsync(ct).ConfigureAwait(false);
+                    }
+                    catch (OperationCanceledException ex)
+                    {
+                        if (ex.CancellationToken != ct)
+                        {
+                            observer.OnError(ex);
+                        }
+
+                        return Disposable.Empty;
+                    }
+                }
+
+                return Disposable.Create(async () =>
+                {
+                    sensor.ReadingChanged -= h;
+
+                    if (startAndStopSensorReadings)
+                    {
+                        //
+                        // NOTE: Alternatively, we could just block on the task. Future releases
+                        // of Rx may support IAsyncDisposable, which would provide a natural fit.
+                        //
+
+                        try
+                        {
+                            //
+                            // If we have an outstanding stop request, await it to avoid races.
+                            //
+                            await stopping.ConfigureAwait(false);
+
+                            //
+                            // Initiate the stop operation and track it.
+                            //
+                            stopping = sensor.StopReadingsAsync();
+                            await stopping.ConfigureAwait(false);
+                        }
+                        finally
+                        {
+                            //
+                            // Unblock future awaits for the stop operation.
+                            //
+                            stopping = Task.FromResult(true);
+                        }
+                    }
+                });
+            });
+
+            //
+            // Try to avoid attaching multiple event handlers and excessive start and stop
+            // requests to the underlying sensor. Even though it seems the Band SDK deals
+            // with this at a lower level, this allows fan-out to multiple subscriptions
+            // within the Rx layer.
+            //
+            res = res.Publish().RefCount();
+
+            return res;
+        }
+    }
+}
